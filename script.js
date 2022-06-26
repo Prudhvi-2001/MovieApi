7701b44ba34f84f5d700c9bbd48d9efc482e266a
Kuntrapakam Muni Prudhvi <85389980+Prudhvi-2001@users.noreply.github.com>
2022-06-26 13:39:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ function showMovies(data) {
                 <h3>Overview</h3>
                 ${overview}
                 <br/> 
-                <button class="know-more" id="${id}">Know More</button
+                <button class="know-more" id="${id}">Watch Trailer</button
             </div>
         
         `
